c199528b69b25165a3c7320a42f39ba6e797bd26
CoolEnoughSmile <2398140855@qq.com>
2019-06-30 17:01:15 +0800

    答辩最终版

--- src/main/java/ces/ge/music/entity/Video.java
+++ src/main/java/ces/ge/music/entity/Video.java
@@ -5,18 +5,20 @@ public class Video {
     private String videoName;
     private String poster;
     private String url;
-    private String palyCount;
+    private String playCount;
     private String singer;
+    private int videoId;
 
     public Video() {
     }
 
-    public Video(String videoName, String poster, String url, String palyCount, String singer) {
+    public Video(String videoName, String poster, String url, String playCount, String singer,int videoId) {
         this.videoName = videoName;
         this.poster = poster;
         this.url = url;
-        this.palyCount = palyCount;
+        this.playCount = playCount;
         this.singer = singer;
+        this.videoId = videoId;
     }
 
     public String getVideoName() {
@@ -43,12 +45,12 @@ public class Video {
         this.url = url;
     }
 
-    public String getPalyCount() {
-        return palyCount;
+    public String getPlayCount() {
+        return playCount;
     }
 
-    public void setPalyCount(String palyCount) {
-        this.palyCount = palyCount;
+    public void setPlayCount(String playCount) {
+        this.playCount = playCount;
     }
 
     public String getSinger() {
@@ -59,14 +61,23 @@ public class Video {
         this.singer = singer;
     }
 
+    public int getVideoId() {
+        return videoId;
+    }
+
+    public void setVideoId(int videoId) {
+        this.videoId = videoId;
+    }
+
     @Override
     public String toString() {
         return "Video{" +
                 "videoName='" + videoName + '\'' +
                 ", poster='" + poster + '\'' +
                 ", url='" + url + '\'' +
-                ", palyCount='" + palyCount + '\'' +
+                ", playCount='" + playCount + '\'' +
                 ", singer='" + singer + '\'' +
+                ", videoId='" + videoId + '\'' +
                 '}';
     }
 
